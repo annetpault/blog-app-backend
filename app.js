0f8277457712572428d4abda22c469e887deb706
annetpault <annetmca25@gmail.com>
2026-07-15 23:34:47 +0530

    SignUp completed

--- app.js
+++ app.js
@@ -3,11 +3,43 @@ const Mongoose = require("mongoose")
 const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
 let app= Express()
 
-app.get("/",(req,res)=> {
-    res.send("hello")
+app.use(Express.json())
+app.use(Cors())
+
+Mongoose.connect("mongodb://annet-paul:annet123@ac-rrnjxoo-shard-00-00.bcqym4j.mongodb.net:27017,ac-rrnjxoo-shard-00-01.bcqym4j.mongodb.net:27017,ac-rrnjxoo-shard-00-02.bcqym4j.mongodb.net:27017/blogAppDb?ssl=true&replicaSet=atlas-stpu0i-shard-0&authSource=admin&appName=Cluster0")
+
+app.post("/signup",async(req,res)=> {
+    
+    let input=req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password,10)
+    console.log(hashedPassword)
+    req.body.password=hashedPassword
+
+   userModel.find({email:req.body.email}).then(
+    (items) => {
+        
+    if (items.length>0) {
+            
+            res.json({"status":"email Id already exists"})
+
+        } else {
+
+            let result= new userModel(input)
+            result.save()
+            res.json({"status":"success"})
+
+        }
+        
+    }
+   ).catch(
+    (error) =>{}
+   )
+
+    
 })
 
 app.listen(3030,()=>{
