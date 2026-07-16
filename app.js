cbfbdd7bf632b61697c80aecd995e87d8fe8299f
annetpault <annetmca25@gmail.com>
2026-07-16 21:14:50 +0530

    Viewall completed

--- app.js
+++ app.js
@@ -35,6 +35,32 @@ app.post("/create",async(req,res)=>{
 
 })
 
+//  View all
+app.post("/viewall",(req,res)=>{
+
+    let token=req.headers.token
+
+    jwt.verify(token,"blogApp",(error,decoded)=>{
+        if (decoded && decoded.email) {
+
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+            
+        } else {
+            
+        }
+    })
+
+    
+})
+
 // Sign In
 app.post("/signIn",async(req,res)=> {
 
